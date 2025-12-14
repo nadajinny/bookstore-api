ae5acc6d3322e764f6d57763a35ff502572ed43f
nadajinny <jinsun23007@gmail.com>
2025-12-14 15:46:18 +0900

    feat: health controller 정리

--- src/main/java/com/example/bookstore_api/health/HealthController.java
+++ src/main/java/com/example/bookstore_api/health/HealthController.java
@@ -1,30 +1,19 @@
 package com.example.bookstore_api.health;
 
-import com.example.bookstore_api.user.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 import java.util.Map;
 
 @RestController
-@RequiredArgsConstructor
 public class HealthController {
-
-    private final UserRepository userRepository;
 
     @GetMapping("/health")
     public Map<String, Object> health() {
         return Map.of(
                 "status", "UP",
-                "time", LocalDateTime.now()
+                "timestamp", Instant.now().toString()
         );
-    }
-
-    @GetMapping("/health/db")
-    public String dbHealth() {
-        userRepository.count();
-        return "DB OK";
     }
 }
